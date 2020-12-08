03b6a3e8f638399a12fdeaaf1e3783e3d62ff32f
cwesterink <constantine.westerink@gmail.com>
2020-12-08 12:36:51 -0800

    s

--- FFAS.py
+++ FFAS.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ['POST'])
 def index():
 	return render_template("index.html")
 
